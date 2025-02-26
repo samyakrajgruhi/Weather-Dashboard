c394cc4c18f04943a280821e36d5e96230525217
Samyak Rajgruhi <samyakrajgruhi@gmail.com>
2025-02-26 18:50:26 +0530

    PC display done

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  server: {
+    allowedHosts: ['8045-122-161-67-131.ngrok-free.app']
+  }
 })
